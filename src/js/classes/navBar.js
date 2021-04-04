ea3988f1fa3ec565b76435d969e2cd682198171d
anthony-dee <anthonydouglas525@gmail.com>
2021-04-04 10:51:54 +0100

    refactored html - removed outer ul nav list

--- src/js/classes/navBar.js
+++ src/js/classes/navBar.js
@@ -8,11 +8,19 @@ export default class NavBar {
 
       this.navBar.classList.remove('no-js');
 
-      this.menu = this.navBar.querySelector('.nav-main');
+      this.menuToggle = this.navBar.querySelector('.nav-menu-toggle');
+
+      this.menu = this.navBar.querySelector('.nav-main-menu');
+
+      if (window.innerWidth >= 768) {
+        this.menu.setAttribute('aria-hidden', 'false');
+      }
 
       this.setMenuHeight();
 
-      this.menu.setAttribute('style', `--nav-height: ${this.getMenuHeight()}px`)
+      this.baseMenuStyle = `--nav-height: ${this.getMenuHeight()}px; `;
+
+      this.menu.setAttribute('style', this.baseMenuStyle)
 
       this.animation = null;
 
@@ -23,22 +31,24 @@ export default class NavBar {
       this.currentMenuButton = false;
 
       this.registerListeners();
-
-      this.start = null;
   }
 
   registerListeners() {
     this.navBar.addEventListener('click', (event) => {
       if (event.target.closest('.nav-menu-toggle')) {
-        console.log('hello')
-        const menuToggle = event.target.closest('.nav-menu-toggle');
-        if (menuToggle.getAttribute('aria-expanded') === 'false') {
-          menuToggle.setAttribute('aria-expanded', 'true');
+        const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+        if (this.menuToggle.getAttribute('aria-expanded') === 'false') {
+          this.menuToggle.setAttribute('aria-expanded', 'true');
           this.menu.setAttribute('aria-hidden', 'false');
         } else {
-          menuToggle.setAttribute('aria-expanded', 'false');
+          this.menuToggle.setAttribute('aria-expanded', 'false');
           this.menu.setAttribute('aria-hidden', 'true');
         }
+
+        /*if (!prefersReducedMotion.matches) {
+          this.toggleBtnClick(event);
+        }*/
       }
 
       if (event.target.closest('.nav-dropdown-toggle')) {
@@ -55,6 +65,16 @@ export default class NavBar {
 
     document.addEventListener( 'click', (e) => {
       this.closeOpenMenu(e);
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth >= 768) {
+        this.menuToggle.setAttribute('aria-expanded', 'true');
+        this.menu.setAttribute('aria-hidden', 'false');
+      } else {
+        this.menuToggle.setAttribute('aria-expanded', 'false');
+        this.menu.setAttribute('aria-hidden', 'true');
+      }
     });
   }
 
@@ -159,6 +179,6 @@ export default class NavBar {
   }
 
   setMenuHeight() {
-    this.menuHeight = this.menu.querySelector('.nav-list').offsetHeight;
+    this.menuHeight = this.menu.offsetHeight;
   }
 }
