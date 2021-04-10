6982545d9392c93164314ec089272efa6c6b5981
anthony-dee <anthonydouglas525@gmail.com>
2021-04-10 18:35:53 +0100

    reformatted navBar.js so all tabs are 4 spaces

--- src/js/classes/navBar.js
+++ src/js/classes/navBar.js
@@ -3,185 +3,185 @@ import '../../scss/test.scss';
 
 export default class NavBar {
 
-  constructor(navBar) {
-      this.navBar = navBar;
-      this.navBar.classList.remove('no-js');
-      this.menuToggle = this.navBar.querySelector('.nav-menu-toggle');
-      this.menu = this.navBar.querySelector('.nav-main-menu');
+    constructor(navBar) {
+        this.navBar = navBar;
+        this.navBar.classList.remove('no-js');
+        this.menuToggle = this.navBar.querySelector('.nav-menu-toggle');
+        this.menu = this.navBar.querySelector('.nav-main-menu');
 
-      if (window.innerWidth >= 768) {
-        this.menu.setAttribute('aria-hidden', 'false');
-      }
+        if (window.innerWidth >= 768) {
+            this.menu.setAttribute('aria-hidden', 'false');
+        }
 
-      this.prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-      this.transitionTime = this.prefersReducedMotion ? 10 : 400;
-      this.setMenuHeight();
+        this.prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+        this.transitionTime = this.prefersReducedMotion ? 10 : 400;
+        this.setMenuHeight();
 
-      document.documentElement.style.setProperty('--nav-height', `${this.getMenuHeight()}px`);
+        document.documentElement.style.setProperty('--nav-height', `${this.getMenuHeight()}px`);
 
-      this.easing = 'ease';
-      this.animation = null;
-      this.isNavMenuOpen = false;
-      this.isClosing = false;
-      this.isExpanding = false;
-      this.currentMenuButton = false;
-      this.registerListeners();
-      this.calculateOpenMenuHeight();
-  }
+        this.easing = 'ease';
+        this.animation = null;
+        this.isNavMenuOpen = false;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.currentMenuButton = false;
+        this.registerListeners();
+        this.calculateOpenMenuHeight();
+    }
 
-  registerListeners() {
-    this.navBar.addEventListener('click', (event) => {
-      if (event.target.closest('.nav-menu-toggle')) {
-        const menuWasClosed = this.menuToggle.getAttribute('aria-expanded') === 'false';
+    registerListeners() {
+        this.navBar.addEventListener('click', (event) => {
+            if (event.target.closest('.nav-menu-toggle')) {
+                const menuWasClosed = this.menuToggle.getAttribute('aria-expanded') === 'false';
 
-        if (menuWasClosed) {
-          this.menuToggle.setAttribute('aria-expanded', 'true');
-          this.menu.setAttribute('aria-hidden', 'false');
+                if (menuWasClosed) {
+                    this.menuToggle.setAttribute('aria-expanded', 'true');
+                    this.menu.setAttribute('aria-hidden', 'false');
+                } else {
+                    this.menuToggle.setAttribute('aria-expanded', 'false');
+                    this.menu.setAttribute('aria-hidden', 'true');
+                }
+
+                this.toggleMainMenu(event);
+            }
+
+            if (event.target.closest('.nav-dropdown-toggle')) {
+                const button = event.target;
+
+                // close an open dropdown if there is one
+                if (this.currentMenuButton && this.currentMenuButton !== button) {
+                    this.toggleDropdown(this.currentMenuButton);
+                }
+
+                this.toggleDropdown(button);
+            }
+        }, false);
+
+        document.addEventListener('click', (e) => {
+            this.closeOpenMenu(e);
+        });
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth >= 768) {
+                this.menuToggle.setAttribute('aria-expanded', 'true');
+                this.menu.setAttribute('aria-hidden', 'false');
+                this.menu.removeAttribute('style');
+            } else {
+                if (!this.isNavMenuOpen) {
+                    this.menuToggle.setAttribute('aria-expanded', 'false');
+                    this.menu.setAttribute('aria-hidden', 'true');
+                } else {
+                    this.menu.style.height = 'auto';
+                }
+            }
+        });
+    }
+
+    closeOpenMenu(e) {
+        if (this.currentMenuButton && !e.target.closest('.nav-list-dropdown')) {
+            this.toggleDropdown(this.currentMenuButton);
+        }
+    }
+
+    toggleDropdown(button) {
+        const dropdown = document.getElementById(button.getAttribute('aria-controls'));
+        if ('true' === button.getAttribute('aria-expanded')) {
+            button.setAttribute('aria-expanded', 'false');
+            dropdown.setAttribute('aria-hidden', 'true')
+            this.currentMenuButton = false;
         } else {
-          this.menuToggle.setAttribute('aria-expanded', 'false');
-          this.menu.setAttribute('aria-hidden', 'true');
+            button.setAttribute('aria-expanded', 'true');
+            dropdown.setAttribute('aria-hidden', 'false')
+            this.currentMenuButton = button;
+        }
+    }
+
+// Function called when user clicks on the nav menu toggle
+    toggleMainMenu(event) {
+        event.preventDefault();
+
+        if (this.isClosing || !this.isNavMenuOpen) {
+            this.openMenu();
+        } else if (this.isExpanding || this.isNavMenuOpen) {
+            this.closeMenu();
+        }
+    }
+
+    closeMenu() {
+        this.isClosing = true;
+        const startHeight = `${this.menu.offsetHeight}px`;
+        const endHeight = '0px';
+
+        if (this.animation) {
+            this.animation.cancel();
         }
 
-        this.toggleMainMenu(event);
-      }
+        this.animation = this.menu.animate({
+            height: [startHeight, endHeight]
+        }, {
+            duration: this.transitionTime, easing: this.easing
+        });
 
-      if (event.target.closest('.nav-dropdown-toggle')) {
-        const button = event.target;
+        this.menu.style.height = endHeight;
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+        this.animation.oncancel = () => this.isClosing = false;
+    }
 
-        // close an open dropdown if there is one
-        if (this.currentMenuButton && this.currentMenuButton !== button) {
-          this.toggleDropdown(this.currentMenuButton);
+// Function called to open the element after click
+    openMenu() {
+        this.isNavMenuOpen = true;
+        window.requestAnimationFrame(() => this.expand());
+    }
+
+// Function called to expand the content with an animation
+    expand() {
+        this.isExpanding = true;
+        const startHeight = `0px`;
+        const endHeight = `${this.menuHeight}px`;
+
+        if (this.animation) {
+            this.animation.cancel();
         }
 
-        this.toggleDropdown(button);
-      }
-    }, false);
+        this.animation = this.menu.animate({
+            height: [startHeight, endHeight]
+        }, {
+            duration: this.transitionTime, easing: this.easing
+        });
 
-    document.addEventListener( 'click', (e) => {
-      this.closeOpenMenu(e);
-    });
+        this.menu.style.height = endHeight;
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+        this.animation.oncancel = () => this.isExpanding = false;
+    }
 
-    window.addEventListener('resize', () => {
-      if (window.innerWidth >= 768) {
-        this.menuToggle.setAttribute('aria-expanded', 'true');
-        this.menu.setAttribute('aria-hidden', 'false');
-        this.menu.removeAttribute('style');
-      } else {
-        if (!this.isNavMenuOpen) {
-          this.menuToggle.setAttribute('aria-expanded', 'false');
-          this.menu.setAttribute('aria-hidden', 'true');
-        } else {
-          this.menu.style.height = 'auto';
+// Callback when the shrink or expand animations are done
+    onAnimationFinish(isNavMenuOpen) {
+        this.isNavMenuOpen = isNavMenuOpen;
+        this.menu.classList.toggle('menu-open', this.isNavMenuOpen);
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+
+        if (this.isNavMenuOpen) {
+            this.menu.style.height = 'auto';
         }
-      }
-    });
-  }
-
-  closeOpenMenu(e) {
-    if (this.currentMenuButton && !e.target.closest( '.nav-list-dropdown')) {
-      this.toggleDropdown(this.currentMenuButton);
-    }
-  }
-
-  toggleDropdown(button) {
-    const dropdown = document.getElementById(button.getAttribute('aria-controls'));
-    if ('true' === button.getAttribute( 'aria-expanded' )) {
-      button.setAttribute('aria-expanded', 'false');
-      dropdown.setAttribute('aria-hidden', 'true')
-      this.currentMenuButton = false;
-    } else {
-      button.setAttribute('aria-expanded', 'true');
-      dropdown.setAttribute('aria-hidden', 'false')
-      this.currentMenuButton = button;
-    }
-  }
-
-  // Function called when user clicks on the nav menu toggle
-  toggleMainMenu(event) {
-    event.preventDefault();
-
-    if (this.isClosing || !this.isNavMenuOpen) {
-      this.openMenu();
-    } else if (this.isExpanding || this.isNavMenuOpen) {
-      this.closeMenu();
-    }
-  }
-
-  closeMenu() {
-    this.isClosing = true;
-    const startHeight = `${this.menu.offsetHeight}px`;
-    const endHeight = '0px';
-
-    if (this.animation) {
-      this.animation.cancel();
     }
 
-    this.animation = this.menu.animate({
-      height: [startHeight, endHeight]
-    }, {
-      duration: this.transitionTime, easing: this.easing
-    });
-
-    this.menu.style.height = endHeight;
-    this.animation.onfinish = () => this.onAnimationFinish(false);
-    this.animation.oncancel = () => this.isClosing = false;
-  }
-
-  // Function called to open the element after click
-  openMenu() {
-    this.isNavMenuOpen = true;
-    window.requestAnimationFrame(() => this.expand());
-  }
-
-  // Function called to expand the content with an animation
-  expand() {
-    this.isExpanding = true;
-    const startHeight = `0px`;
-    const endHeight = `${this.menuHeight}px`;
-
-    if (this.animation) {
-      this.animation.cancel();
+    getMenuHeight() {
+        return this.menuHeight;
     }
 
-    this.animation = this.menu.animate({
-      height: [startHeight, endHeight]
-    }, {
-      duration: this.transitionTime, easing: this.easing
-    });
-
-    this.menu.style.height = endHeight;
-    this.animation.onfinish = () => this.onAnimationFinish(true);
-    this.animation.oncancel = () => this.isExpanding = false;
-  }
-
-  // Callback when the shrink or expand animations are done
-  onAnimationFinish(isNavMenuOpen) {
-    this.isNavMenuOpen = isNavMenuOpen;
-    this.menu.classList.toggle('menu-open', this.isNavMenuOpen);
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding =  false;
-
-    if (this.isNavMenuOpen) {
-       this.menu.style.height = 'auto';
-    }
-  }
-
-  getMenuHeight() {
-    return this.menuHeight;
-  }
-
-  setMenuHeight() {
-    this.menuHeight = this.calculateOpenMenuHeight();
-  }
-
-  calculateOpenMenuHeight() {
-    let height = 0;
-
-    for (let i = 0; i < this.menu.children.length; i++) {
-      height += this.menu.children[i].offsetHeight;
+    setMenuHeight() {
+        this.menuHeight = this.calculateOpenMenuHeight();
     }
 
-    return height;
-  }
+    calculateOpenMenuHeight() {
+        let height = 0;
+
+        for (let i = 0; i < this.menu.children.length; i++) {
+            height += this.menu.children[i].offsetHeight;
+        }
+
+        return height;
+    }
 }
